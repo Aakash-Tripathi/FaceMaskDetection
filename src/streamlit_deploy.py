462b891cb8ab996488734fa0682974873dffe585
Aakash Tripathi <aakash.tripathi0304@gmail.com>
2021-05-18 01:40:48 -0400

    Add prediction file & started streamlit integration

--- src/streamlit_deploy.py
+++ src/streamlit_deploy.py
@@ -7,9 +7,11 @@ def main():
         Add Instructions here
         """
     st.write(instructions)
-    file = st.file_uploader('Upload An Image')
-    st.title("Here is the image you've selected")
-    st.title('How it works:')
+    file = st.file_uploader('')
+
+    if file:
+        st.image(file)
+        st.success('Success message')
 
 
 if __name__ == '__main__':
