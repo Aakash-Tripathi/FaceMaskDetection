d15928bfcbca46776e8af3a87e79ad3ce82b869d
Aakash Tripathi <aakash.tripathi0304@gmail.com>
2021-07-05 22:48:02 -0400

    documentation with the rtd theme

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,3 @@
-import sphinx_rtd_theme
-
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
@@ -12,9 +10,9 @@ import sphinx_rtd_theme
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../src'))
 
 
 # -- Project information -----------------------------------------------------
@@ -33,15 +31,16 @@ release = '0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "sphinx_rtd_theme",
+    'sphinx.ext.autodoc'
 ]
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
--- docs/index.rst
+++ docs/index.rst
@@ -1,10 +1,13 @@
 .. Face Mask Detection documentation master file, created by
-   sphinx-quickstart on Tue May 25 18:45:51 2021.
+   sphinx-quickstart on Mon Jul  5 22:39:37 2021.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
 Welcome to Face Mask Detection's documentation!
 ===============================================
+
+.. automodule:: loader
+   :members:
 
 .. toctree::
    :maxdepth: 2
